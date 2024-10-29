76ceeb7c8811da9e03b1fa3331887ad613a9cc26
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-10-30 00:24:53 +0545

    add hero content and stylize hero and header section

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,15 +1,36 @@
 import homePageBgImage from "../image/homepage-background.jpg";
 
 const loadHomePage = () => {
+  // Prepare hero content section
+  const heroContent = document.getElementById("hero-content");
+  heroContent.innerHTML = "";
+
+  const heroH1 = document.createElement("h1");
+  const heroH2 = document.createElement("h2");
+  const heroP = document.createElement("p");
+  const heroOrderBtn = document.createElement("button");
+
+  heroH1.textContent = "ROYAL KITCHEN";
+  heroH2.textContent = "Experience the Taste of Royalty";
+  heroP.textContent =
+    "At Royal Kitchen, we believe dining is more than just a meal—it’s an experience crafted with passion and precision. From the finest ingredients to exquisite presentation, every dish is a celebration of elegance and flavor. Join us for a journey that blends traditional culinary artistry with a touch of modern sophistication. Your royal feast awaits.";
+  heroOrderBtn.textContent = "Order Now";
+
+  heroContent.appendChild(heroH1);
+  heroContent.appendChild(heroH2);
+  heroContent.appendChild(heroP);
+  heroContent.appendChild(heroOrderBtn);
+
+  // Prepare Content Section
   const content = document.getElementById("content");
   content.innerHTML = "";
 
-  const bgImgEle = document.createElement("img");
-  bgImgEle.src = homePageBgImage;
-  bgImgEle.alt = "A cozy view of the restaurant";
-  bgImgEle.classList.add("background-image");
+  // const bgImgEle = document.createElement("img");
+  // bgImgEle.src = homePageBgImage;
+  // bgImgEle.alt = "A cozy view of the restaurant";
+  // bgImgEle.classList.add("background-image");
 
-  content.appendChild(bgImgEle);
+  // content.appendChild(bgImgEle);
 };
 
 export default loadHomePage;
