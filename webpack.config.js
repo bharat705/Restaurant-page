a276c226b59f7dca4334923c772519f55a7e677a
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-10-29 22:11:39 +0545

    Add and setup custom fonts

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // Match font file extensions
+        type: "asset/resource", // Handles font files as separate assets
+      },
     ],
   },
 };
