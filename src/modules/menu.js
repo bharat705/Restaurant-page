6b0004a0edfb8c7152815a12c3a572fa2b6baa75
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-11-10 21:37:10 +0545

    fixed images not showing on menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,64 +1,76 @@
+// Import images directly if using Webpack
+import spaghettiImage from "../image/spaghetti.jpg";
+import pizzaImage from "../image/pizza.jpg";
+import caesarSaladImage from "../image/caesar_salad.jpg";
+import salmonImage from "../image/salmon.jpg";
+import steakImage from "../image/steak.jpg";
+import tiramisuImage from "../image/tiramisu.jpg";
+import alfredoImage from "../image/alfredo.jpg";
+import onionSoupImage from "../image/french_onion_soup.jpg";
+import scampiImage from "../image/scampi.jpg";
+import lavaCakeImage from "../image/lava_cake.jpg";
+
 const menuItems = [
   {
     name: "Spaghetti Carbonara",
     description: "Classic Italian pasta with creamy sauce and pancetta.",
     price: "$12.99",
-    image: "../image/spaghetti.jpg",
+    image: spaghettiImage,
   },
   {
     name: "Margherita Pizza",
     description: "Fresh tomatoes, mozzarella, and basil on a thin crust.",
     price: "$10.99",
-    image: "../image/pizza.jpg",
+    image: pizzaImage,
   },
   {
     name: "Caesar Salad",
     description: "Crisp romaine lettuce with Caesar dressing and croutons.",
     price: "$8.99",
-    image: "../image/caesar_salad.jpg",
+    image: caesarSaladImage,
   },
   {
     name: "Grilled Salmon",
     description: "Perfectly grilled salmon with lemon butter sauce.",
     price: "$15.99",
-    image: "../image/salmon.jpg",
+    image: salmonImage,
   },
   {
     name: "Ribeye Steak",
     description: "Juicy ribeye steak cooked to perfection.",
     price: "$19.99",
-    image: "../image/steak.jpg",
+    image: steakImage,
   },
   {
     name: "Tiramisu",
     description: "Traditional Italian dessert with coffee and mascarpone.",
     price: "$6.99",
-    image: "../image/tiramisu.jpg",
+    image: tiramisuImage,
   },
   {
     name: "Chicken Alfredo",
     description:
       "Fettuccine pasta with creamy Alfredo sauce and grilled chicken.",
     price: "$13.99",
-    image: "../image/alfredo.jpg",
+    image: alfredoImage,
   },
   {
     name: "French Onion Soup",
     description: "Rich beef broth with caramelized onions and melted cheese.",
     price: "$7.99",
-    image: "../image/french_onion_soup.jpg",
+    image: onionSoupImage,
   },
   {
     name: "Shrimp Scampi",
     description: "Succulent shrimp sautéed in garlic butter and white wine.",
     price: "$14.99",
-    image: "../image/scampi.jpg",
+    image: scampiImage,
   },
   {
     name: "Chocolate Lava Cake",
     description: "Warm chocolate cake with a gooey molten center.",
     price: "$5.99",
-    image: "../image/lava_cake.jpg",
+    image: lavaCakeImage,
   },
 ];
 
